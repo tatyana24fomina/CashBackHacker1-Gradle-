77ec22c7109d86d1ecf208833631a2a60aa97e70
Tatyana Fomina <tatyana24fomina@gmail.com>
2022-11-10 16:27:22 +0400

    Init testNG

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,49 @@
+package ru.netology;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldRemainAmount() {
+        int amount = 1500;
+        int expected = 500;
+
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldNegativeCheckRemainAmount() {
+        int amount = 1500;
+        int expected = 600;
+
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertNotEquals(actual, expected);
+    }
+
+    @Test
+    public void should_Remain_when_AmountIs1000() {
+        int amount = 1000;
+        int expected = 0;
+
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void should_Remain_when_Amount_is_less_1000() {
+        int amount = 700;
+        int expected = 300;
+
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+}
